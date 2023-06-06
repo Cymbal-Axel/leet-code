205e7661b85812841bcd0b36be8041251ac61285
Axel <cymbalaxel@gmail.com>
2023-06-06 18:17:37 -0300

    maxConsecutiveOnes

---
+++ maxConsecutiveOnes.js
@@ -0,0 +1,41 @@
+/*
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+    Input: nums = [1,1,0,1,1,1]
+    Output: 3
+    Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+
+Example 2:
+    Input: nums = [1,0,1,1,0,1]
+    Output: 2
+
+
+Constraints:
+
+    1 <= nums.length <= 105
+    nums[i] is either 0 or 1.
+
+*/
+const case1 = [1,1,0,1,1,1]; //Expected   3
+const case2 = [1,0,1,1,0,1]; //Expected   2
+
+var findMaxConsecutiveOnes = function(nums) {
+    let max = 0;
+    let current = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === 1){
+            current += 1;
+        } else {
+            current = 0
+        }
+        if(current > max){
+            max = current;
+        }
+    }
+    return max
+};
+
+
